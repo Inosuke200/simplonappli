8aeff00b08ec27d44f889dae777b5068738be193
kagami200 <ismaelouiya@gmail.com>
2021-02-01 21:59:16 +0100

    version0

---
+++ connexion.php
@@ -0,0 +1,92 @@
+<?php 
+    session_start();
+    require_once 'config.php';
+?>
+<!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+            <link rel="stylesheet" href="styles/style.css">
+            <title>Connexion</title>
+        </head>
+        <body>
+            <div class="container">
+        
+                <div class="text-center">
+                    <?php 
+                    if(isset($_POST['email']) && isset($_POST['password']) )
+                    {
+                        $email = htmlspecialchars($_POST['email']);
+                        $password = htmlspecialchars($_POST['password']);
+
+                            if(filter_var($email, FILTER_VALIDATE_EMAIL))
+                            {
+
+                                    $check = $bdd->prepare('SELECT email, password FROM utilisateurs WHERE email = ?');
+                                    $check->execute(array($email));
+                                    $data = $check->fetch();
+                                    $row = $check->rowCount();
+
+                                    if($row == 1)
+                                    {
+                                        $password = hash('sha256', $password);
+
+                                        if($password == $data['password'])
+                                        {
+                                            $_SESSION['user'] = $email;
+                                            header('Location:index.php');
+                                        }
+                                        else 
+                                            echo "Mot de passe incorrect";
+
+                                    }
+                                    else 
+                                        echo "Ce compte n'existe pas !";
+
+                            }
+                            else 
+                                echo "Email non valide !";
+
+                    }
+                ?>
+                </div>
+                <br>
+                <div class="all" style="display:flex;">
+                    <div class="image" style="background-color:#f9f9f9; width:80%; height:430px;">
+                            <div class="row">
+                                <div class="col-lg-4 col-sm-4 col-md-4 col-xs-4 offset-4 mt-5">
+                                    <img src="images/simplone.png" alt=""  width="80%" height="180px" style="">
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-lg-12 col-xs-12 col-sm-12 col-md-12">
+                                <h1 class="text-uppercase text-primary font-weight-bold text-center">Bienvenue</h1>
+                                <h2 class="font-weight-bold text-center">sur le portail de <span class="text-danger">SIMPLON'APS !</span> </h2>
+                                </div>
+                            </div>
+                    </div>
+                    <div class="form-group" style="background-color:#00babb; width:80%; height:430px;">
+                        <h1 class="py-3" style="font-family:italic;">Se connecter</h1>
+                            <form action="" method="POST">
+                                <input type="email" name="email" class="form-control" placeholder="Votre email" autocomplete="off" required>
+                                <br>
+                                <input type="password" name="password" class="form-control" placeholder="Votre mot de passe" autocomplete="off" required>
+                                <br>
+                                <br>
+                                <button type="submit" class="btn btn-success">Connexion</button>
+                            </form>
+                            <p class="mt-5 font-size-3">Avez-vous déjà un compte ?<a href="inscription.php"> S'inscrire</a></p>
+                   </div>
+                </div>
+            </div>
+            <p class="text-white text-center mt-5">2019 -2021 | Copyright | SIMPLON'APS, Tous droits réservés
+</p>
+
+        </body>
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</html> 
---
+++ utilisateurs.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : ven. 29 jan. 2021 à 18:26
+-- Version du serveur :  10.4.17-MariaDB
+-- Version de PHP : 8.0.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `simplon`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateurs`
+--
+
+CREATE TABLE `utilisateurs` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(100) NOT NULL,
+  `prenom` varchar(100) NOT NULL,
+  `email` text NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `password_repeat` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `utilisateurs`
+--
+
+INSERT INTO `utilisateurs` (`id`, `nom`, `prenom`, `email`, `password`, `password_repeat`) VALUES
+(1, 'GUIGMA', 'alain', 'guigmaalain@gmail.com', '926b4b8a00cfab44b758', '0000-00-00 00:00:00'),
+(2, 'TAPSOBA', 'Nafissatou', 'nafissatoutapsoba08@gmail.com', '926b4b8a00cfab44b758', '0000-00-00 00:00:00');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
